eb552c18c5f4bf328f201a7de57360c4f9cbb1b7
sOubHik <soubhik93@gmail.com>
2021-05-18 09:59:22 +0530

    Add sip form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AddMfDetails from './mf/add-details';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddMfDetails />
   </React.StrictMode>,
   document.getElementById('root')
 );
